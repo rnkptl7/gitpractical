7fe7404e3df4cfa17f43b2a83466115f8a663589
Ronak Patel <ronak.patel@simformsolutions.com>
2023-02-15 16:08:33 +0530

    Add a writeData functionality

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello New Branch")
+console.log("Hello New Branch");
+
+function writeData(name, age) {
+    console.log(`Hello my name is ${name} and age is ${age}`)
+}
+
+writeData('Ronak Patel', 22);
